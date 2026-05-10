a497fb4639f878cf3c6ad574874a67c6d09625b4
Ascoor <94120646+Ascoor@users.noreply.github.com>
2026-05-10 04:49:41 +0300

    Update config.jsx

--- avocat-frontend/src/config/config.jsx
+++ avocat-frontend/src/config/config.jsx
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000',
   // baseURL: 'https://avocat.ask-ar.net/avocatapp',
 };
 
