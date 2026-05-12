bb46739e7dac8b8b49246d6cadac778860e46ade
Ascoor <94120646+Ascoor@users.noreply.github.com>
2026-05-12 04:11:21 +0300

    Update cors.php

--- avocat-backend/config/cors.php
+++ avocat-backend/config/cors.php
@@ -7,6 +7,8 @@ return [
 
     'allowed_origins' => explode(',', env(
         'FRONTEND_URLS',
+
+        'https://avocat-frontend-bhbi-production.up.railway.app',
         'http://localhost:8080,http://localhost:5173,http://localhost:3000'
     )),
 
